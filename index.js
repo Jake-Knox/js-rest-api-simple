d8607ac5f7ea69b3d2954d80a716073b6f5ea126
jake-knox- <jakeknox11@hotmail.co.uk>
2023-06-14 23:55:55 +0100

    and and update books to API

--- index.js
+++ index.js
@@ -14,17 +14,38 @@ app.get('/api/hello', (req, res) => {
 });
 
 // post - add a new book
-app.post('/api/books', (req, res) => {
-    const newBook = req.body;
+app.post('/api/books/:title/:author', (req, res) => {
+    const title = req.params.title; 
+    const author = req.params.author; 
+    const newBook = {};
 
+    newBook.title = title;
+    newBook.author = author;
     books.push(newBook);
 
     res.send('Book created successfully!');
-})
+});
 
+// put - update existing book
+app.put('/api/books:id', (req, res) => {
+    const bookID = req.params.id;
+    const updatedBook = req.body;
 
+    // find book from the array
+    const bookToUpdate = books.find((book) => book.id === bookID);
+    if(bookToUpdate)
+    {
+        // update book
+        bookToUpdate.title = updatedBook.title || bookToUpdate.title;
+        bookToUpdate.author = updatedBook.author || bookToUpdate.author;
 
+        res.send('Book updated successfully!');
+        console.log(books);
+    } else {
+        res.status(404).send('Book not found!');
+    }
 
+});
 
 // server start
 app.listen(port, () => {
