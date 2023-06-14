4bae1c327ecfd8c50cbd7e4a637af01794336c12
jake-knox- <jakeknox11@hotmail.co.uk>
2023-06-14 23:28:51 +0100

    add new book

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+// basic data storage
+let books = [];
+
+
+// basic hello world
+app.get('/api/hello', (req, res) => {
+    res.send('Hello, World!');
+});
+
+// post - add a new book
+app.post('/api/books', (req, res) => {
+    const newBook = req.body;
+
+    books.push(newBook);
+
+    res.send('Book created successfully!');
+})
+
+
+
+
+
+// server start
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
