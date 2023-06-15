c0f31bbdb231b3b64cb0af7717dcfd266c483993
jake-knox- <jakeknox11@hotmail.co.uk>
2023-06-15 12:23:37 +0100

    fixing issues, testing with curl

--- index.js
+++ index.js
@@ -5,16 +5,21 @@ const port = 3000;
 app.use(express.json());
 
 // basic data storage
-let books = [];
-
+let books = [
+    {
+      title: "Ficciones",
+      author: "Jorge Luis Borges"
+    }
+  ];
 
 // basic hello world
 app.get('/api/hello', (req, res) => {
-    res.send('Hello, World!');
+    res.send(`${books[0].title}, ${books[0].author}`);
 });
 
 // post - add a new book
 app.post('/api/books/:title/:author', (req, res) => {
+    console.log("add book");
     const title = req.params.title; 
     const author = req.params.author; 
     const newBook = {};
@@ -24,10 +29,14 @@ app.post('/api/books/:title/:author', (req, res) => {
     books.push(newBook);
 
     res.send('Book created successfully!');
+
+    // logBooks();
 });
 
 // put - update existing book
-app.put('/api/books:id', (req, res) => {
+app.put('/api/books/:id', (req, res) => {
+    console.log("edit book");
+
     const bookID = req.params.id;
     const updatedBook = req.body;
 
@@ -44,10 +53,16 @@ app.put('/api/books:id', (req, res) => {
     } else {
         res.status(404).send('Book not found!');
     }
-
+    // logBooks();
 });
 
 // server start
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
+
+const logBooks = () => {
+    for(let i = 0; i<books.length; i++){
+        console.log(books[i]);
+    }
+}
